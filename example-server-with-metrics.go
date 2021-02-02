fa74cae9d0f56d0871e5ef0d8b356e3f92ef3b2c
Ondrej Sika <ondrej@ondrejsika.com>
2021-02-03 00:40:08 +0100

    Add histogram example

--- example-server-with-metrics.go
+++ example-server-with-metrics.go
@@ -17,53 +17,72 @@ var counter_requests = prometheus.NewCounter(
 		Name:      "requests",
 		Help:      "Number of requests",
 	})
+var histogram_request_latency = prometheus.NewHistogram(prometheus.HistogramOpts{
+	Namespace: "example",
+	Name:      "request_duration_seconds",
+	Help:      "Request duration in seconds",
+	Buckets:   []float64{.01, .05, .1, .2, .5, 1, 2, 5},
+})
 
 func main() {
 	// Register request
 	prometheus.MustRegister(counter_requests)
+	prometheus.MustRegister(histogram_request_latency)
 
 	// Add /metrics endpoint
 	http.Handle("/metrics", promhttp.Handler())
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		started := time.Now()
 		counter_requests.Add(1)
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
+		histogram_request_latency.Observe(time.Since(started).Seconds())
 	})
 
 	http.HandleFunc("/sleep10", func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(10 * time.Millisecond)
+		started := time.Now()
+		time.Sleep(5 * time.Millisecond)
 		counter_requests.Add(1)
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
+		histogram_request_latency.Observe(time.Since(started).Seconds())
 	})
 
 	http.HandleFunc("/sleep50", func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(50 * time.Millisecond)
+		started := time.Now()
+		time.Sleep(40 * time.Millisecond)
 		counter_requests.Add(1)
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
+		histogram_request_latency.Observe(time.Since(started).Seconds())
 	})
 
 	http.HandleFunc("/sleep100", func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(100 * time.Millisecond)
+		started := time.Now()
+		time.Sleep(90 * time.Millisecond)
 		counter_requests.Add(1)
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
+		histogram_request_latency.Observe(time.Since(started).Seconds())
 	})
 
 	http.HandleFunc("/sleep200", func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(200 * time.Millisecond)
+		started := time.Now()
+		time.Sleep(190 * time.Millisecond)
 		counter_requests.Add(1)
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
+		histogram_request_latency.Observe(time.Since(started).Seconds())
 	})
 
 	http.HandleFunc("/sleep500", func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(500 * time.Millisecond)
+		started := time.Now()
+		time.Sleep(400 * time.Millisecond)
 		counter_requests.Add(1)
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
+		histogram_request_latency.Observe(time.Since(started).Seconds())
 	})
 
 	fmt.Println("Server started.")
